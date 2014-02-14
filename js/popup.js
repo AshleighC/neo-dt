ac0766e0cd1378cf1d55d3299ea358afa8fc00d0
Ashleigh Cushman <ashleigh.cushman@gmail.com>
2014-02-14 10:17:45 -0800

    Made theme preference persistent.

--- js/popup.js
+++ js/popup.js
@@ -1,14 +1,31 @@
-$.getJSON("data/themes.json", function(themes) {
-  var list = "";
-  $.each(themes, function(name, id) {
-    list += "<li id='" + id + "'><img src='http://images.neopets.com/themes/" +
-        id + "/events/trade_offer.png' class='thumbnail'>" + name + "</li>";
-  });
-  $("#themes").html(list);
+var key = "neo-dt";
 
-  $("#themes li").click(function() {
-    $("#themes li").not(this).removeClass("selected");
-    $(this).addClass("selected");
-    $("#current").html($(this).html());
+var select = function(id) {
+  var theme = $("#" + id);
+  $("#themes li").not(theme).removeClass("selected");
+  theme.addClass("selected");
+  $("#current").html(theme.html());
+};
+
+chrome.storage.local.get(key, function (result) {
+  $.getJSON("data/themes.json", function(themes) {
+    var list = "";
+    $.each(themes, function(name, id) {
+      list += "<li id='" + id + "'><img src='http://images.neopets.com/themes/" +
+      id + "/events/trade_offer.png' class='thumbnail'>" + name + "</li>";
+    });
+    $("#themes").html(list);
+
+    if (!$.isEmptyObject(result)) {
+      select(result[key]);
+    }
+
+    $("#themes li").click(function() {
+      var change = {};
+      var id = $(this).attr("id");
+      select(id);
+      change[key] = id;
+      chrome.storage.local.set(change);
+    });
   });
 });
