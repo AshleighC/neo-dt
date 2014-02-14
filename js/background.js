075c87ab82de4739a14dd697d2feba85b2e3bfce
Ashleigh Cushman <ashleigh.cushman@gmail.com>
2014-02-14 12:09:54 -0800

    Added message passing from background.js.

--- js/background.js
+++ js/background.js
@@ -19,7 +19,9 @@ chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
 chrome.storage.onChanged.addListener(function(changes, namespace) {
   chrome.tabs.query({url: "*://*.neopets.com/*"}, function(tabs) {
     $(tabs).each(function(i, tab) {
-      setIcon(tab.id, changes[key].newValue);
+      var theme = changes[key].newValue;
+      setIcon(tab.id, theme);
+      chrome.tabs.sendMessage(tab.id, theme);
     });
   });
 });
