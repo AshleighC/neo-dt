0bc62554309c006c7a73a9011842969262b10712
Ashleigh Cushman <ashleigh.cushman@gmail.com>
2014-02-14 11:06:47 -0800

    Made the page action icon match the theme.

--- js/background.js
+++ js/background.js
@@ -1,5 +1,23 @@
+var key = "neo-dt";
+
+var setIcon = function(tabId, themeId) {
+  var details = {"tabId": tabId, "path": "img/" + themeId + ".png"};
+  chrome.pageAction.setIcon(details);
+};
+
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
   if (tab.url.indexOf(".neopets.com") >= 0) {
+    chrome.storage.local.get(key, function(result) {
+      if (!$.isEmptyObject(result)) {
+        setIcon(tabId, result[key]);
+      }
+    });
     chrome.pageAction.show(tabId);
   }
+});
+
+chrome.storage.onChanged.addListener(function(changes, namespace) {
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    setIcon(tabs[0].id, changes[key].newValue);
+  });
 });
