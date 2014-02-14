f7634c60504e447acb0e26ef223488ff6f51185e
Ashleigh Cushman <ashleigh.cushman@gmail.com>
2014-02-14 06:20:38 -0800

    Fixed flashing problem.

--- js/content.js
+++ js/content.js
@@ -1,16 +1,9 @@
 var theme = "011_alc_c1d1c";
 
 document.addEventListener("DOMNodeInserted", function(ev) {
-  if (ev.relatedNode.localName == "head") {
-    $("link").each(function(i, stylesheet) {
-      var url = $(stylesheet).attr("href");
-      if (url && url.indexOf("themes") > 0) {
-        var i = url.indexOf("?");
-        url = (i > 0) ? url.substring(0, i) : url;
-        url = url.replace(/.{3}_.{3}_.{5}/, theme);
-        $(stylesheet).attr("href", url);
-      }
-    });
+  var html = ev.relatedNode.innerHTML;
+  if ((html.indexOf("/themes/") != -1) && (html.indexOf(theme) == -1)) {
+    ev.relatedNode.innerHTML = html.replace(/.{3}_.{3}_.{5}/, theme);
   }
 }, false);
 
