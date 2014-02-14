fd835414c5b7321119652fba1132c869987801bb
Ashleigh Cushman <ashleigh.cushman@gmail.com>
2014-02-14 11:14:25 -0800

    Made the icon update for all tabs.

--- js/background.js
+++ js/background.js
@@ -17,7 +17,9 @@ chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
 });
 
 chrome.storage.onChanged.addListener(function(changes, namespace) {
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    setIcon(tabs[0].id, changes[key].newValue);
+  chrome.tabs.query({url: "*://*.neopets.com/*"}, function(tabs) {
+    $(tabs).each(function(i, tab) {
+      setIcon(tab.id, changes[key].newValue);
+    });
   });
 });
