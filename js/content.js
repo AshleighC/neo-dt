2c5edcbd0e2ee609adba18a780b1efe60bb1689b
Ashleigh Cushman <ashleigh.cushman@gmail.com>
2014-02-18 05:05:54 -0800

    Rearranged things.

--- js/content.js
+++ js/content.js
@@ -6,11 +6,40 @@ var css;
 var random;
 var rotations;
 
+var imageFixes = {};
+var imageInterval = setInterval(fixImages, 100);
+
 var setVars = function(themeId, newRandom) {
   theme = themeId;
   css = chrome.extension.getURL("css/themes/" + theme + ".css");
+
   if (newRandom || (typeof random == "undefined")) {
     random = Math.random();
+  }
+};
+
+var fixSrc = function(img, path) {
+  img.attr("src", chrome.extension.getURL("img/themes/" + theme + path));
+};
+
+var fixImages = function() {
+  if (theme) {
+    for (element in imageFixes) {
+      imageFixes[element]();
+    }
+  }
+};
+
+var replaceTheme = function(node) {
+  var html = node.innerHTML;
+
+  if (theme) {
+    if (css && !html.match(theme)) {
+      node.innerHTML = html.replace(
+          /http:\/\/images\.neopets\.com\/css\/themes\/.{0,20}\.css/g, css);
+    }
+  } else {
+    chrome.runtime.sendMessage({"theme": html.match(regex)[0]});
   }
 };
 
@@ -21,14 +50,6 @@ chrome.storage.local.get(key, function (result) {
 $.getJSON(chrome.extension.getURL("data/rotations.json"), function(data) {
   rotations = data;
 });
-
-var imageInterval = setInterval(fixImages, 100);
-
-var fixSrc = function(img, path) {
-  img.attr("src", chrome.extension.getURL("img/themes/" + theme + path));
-};
-
-var imageFixes = {};
 
 imageFixes["banner"] = function() {
   var banner = $("#ban");
@@ -49,6 +70,7 @@ imageFixes["banner"] = function() {
 imageFixes["eventIcon"] = function() {
   var eventIcon = $(".eventIcon img");
   var url = eventIcon.attr("src");
+
   if (url) {
     fixSrc(eventIcon, "/events" + url.substring(url.lastIndexOf("/")));
   }
@@ -56,6 +78,7 @@ imageFixes["eventIcon"] = function() {
 
 imageFixes["footerImage"] = function() {
   var footerImg = $(".footerNifty");
+
   if (rotations && footerImg.attr("src")) {
     rotation = Math.floor(random * rotations[theme]) + 1;
     fixSrc(footerImg, "/rotations/" + rotation + ".png");
@@ -66,35 +89,18 @@ imageFixes["navigation"] = function() {
   $(".nav_image img, .copyright img").each(function(i, img) {
     var url = $(img).attr("src");
     var match = url.match(regex);
+
     if (match) {
       fixSrc($(img), url.substr(match.index + match[0].length));
     }
   });
 };
 
-var fixImages = function() {
-  if (theme) {
-    for (element in imageFixes) {
-      imageFixes[element]();
-    }
-  }
-};
-
-var replaceTheme = function(node) {
-  var html = node.innerHTML;
-  if (theme) {
-    if (css && !html.match(theme)) {
-      node.innerHTML = html.replace(
-          /http:\/\/images\.neopets\.com\/css\/themes\/.{0,20}\.css/g, css);
-    }
-  } else {
-    chrome.runtime.sendMessage({"theme": html.match(regex)[0]});
-  }
-};
-
 document.addEventListener("DOMNodeInserted", function(ev) {
-  fixImages();
   var node = ev.relatedNode;
+
+  fixImages();
+
   if (node.localName == "head" && node.innerHTML.match("/themes/")) {
     if (theme === null) {
       chrome.storage.local.get(key, function(result) {
@@ -113,11 +119,13 @@ $(document).ready(function() {
 
 chrome.runtime.onMessage.addListener(function(themeId, sender, sendResponse) {
   setVars(themeId, true);
+  fixImages();
+
   $("link").each(function(i, stylesheet) {
     var url = $(stylesheet).attr("href");
+
     if (url && url.indexOf("themes") > 0) {
       $(stylesheet).attr("href", css);
     }
-    fixImages();
   });
 });
